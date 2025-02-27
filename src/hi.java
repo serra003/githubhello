37252e0d2969daf252a2caa8d4de850fc57a4253
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:45:52 +0100

    removed

--- src/hi.java
+++ src/hi.java
@@ -1,7 +1,6 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("new commit");
         int age;
         age = 20;
         System.out.println(age);
