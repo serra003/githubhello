d7ca8e54d4f16158f7ee320e04b52234e1a2341c
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:44:06 +0100

    newer commit

--- src/hi.java
+++ src/hi.java
@@ -1,7 +1,6 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("Hello World");
         System.out.println("new commit");
 
     }
