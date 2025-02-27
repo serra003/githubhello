9bf28ada1ef93e2755f1ca9cb652df7070bf77be
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:44:53 +0100

    age commit

--- src/hi.java
+++ src/hi.java
@@ -2,6 +2,9 @@ public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
         System.out.println("new commit");
+        int age;
+        age = 20;
+        System.out.println(age);
 
     }
 
