7b5dd1db6ce888a26e1e9ba4ce49b4f474a01d7f
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:42:57 +0100

    removed other lines

--- src/hi.java
+++ src/hi.java
@@ -1,10 +1,7 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("new line");
-        int age;
-        age = 18;
-        System.out.println(age);
+
     }
 
 }
