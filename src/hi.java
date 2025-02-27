bf2f01a9f23ab7f8d90141822a7dce012ffcc81d
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:36:02 +0100

    added another line again

--- src/hi.java
+++ src/hi.java
@@ -1,7 +1,8 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("edited for 3rd commit");
+        System.out.println("this is for 2nd commit");
+        System.out.println("added for 3rd commit");
     }
 
 }
