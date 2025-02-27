8575d0353ace9919a06bfb3f9f266fc1b80a4923
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:41:38 +0100

    added new line

--- src/hi.java
+++ src/hi.java
@@ -1,7 +1,7 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("2nd commit");
+        System.out.println("new line");
     }
 
 }
