9c03646e3c85f329d2964ba27040e2492a7699cc
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:36:28 +0100

    removed lines

--- src/hi.java
+++ src/hi.java
@@ -1,8 +1,6 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("this is for 2nd commit");
-        System.out.println("added for 3rd commit");
     }
 
 }
