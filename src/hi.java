cc3b2a60578c366b404d32dcb0df11fc64873249
Gevher Serra <serraseferoglu@gmail.com>
2025-02-27 16:42:08 +0100

    added age

--- src/hi.java
+++ src/hi.java
@@ -2,6 +2,9 @@ public class hi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
         System.out.println("new line");
+        int age;
+        age = 18;
+        System.out.println(age);
     }
 
 }
